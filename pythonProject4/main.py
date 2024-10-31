eb9b6f34b38f7a8652d7f2a073437e0f49872449
vadim180569 <vadim9771505001@gmail.com>
2024-10-31 19:56:12 +0300

    Add files via upload

---
+++ pythonProject4/main.py
@@ -0,0 +1,8 @@
+#my_string = input("Введите место вашей работы:)
+#print(len("гардероб"))
+my_string = "hello , world"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" , ", ""))
+print(my_string[0])
+print(my_string[-1])
